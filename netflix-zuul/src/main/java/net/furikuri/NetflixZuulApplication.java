e0de91aa074762d145e85d3c0e4750fdf9a33362
Theo Pack Test <tf.pack@gmail.com>
2016-01-30 15:13:10 +0100

    Add zuul proxy

---
+++ netflix-zuul/src/main/java/net/furikuri/NetflixZuulApplication.java
@@ -0,0 +1,14 @@
+package net.furikuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class NetflixZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApplication.class, args);
+	}
+}
